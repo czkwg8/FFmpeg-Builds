1bf1ae103b5b2a84cdcdf1a0125bef8d7d3aafea
R.C. <czkwg8@gmail.com>
2024-12-06 19:26:56 +0800

    Update 50-shaderc.sh

--- scripts.d/50-vulkan/50-shaderc.sh
+++ scripts.d/50-vulkan/50-shaderc.sh
@@ -4,6 +4,7 @@ SCRIPT_REPO="https://github.com/google/shaderc.git"
 SCRIPT_COMMIT="9634bcd356c257e72a06bc995e1d40964d6a8375"
 
 ffbuild_enabled() {
+    [[ $ADDINS_STR == *4.2* ]] && return -1
     [[ $ADDINS_STR == *4.4* ]] && return -1
     return 0
 }
@@ -46,6 +47,7 @@ ffbuild_configure() {
 }
 
 ffbuild_unconfigure() {
+    [[ $ADDINS_STR == *4.2* ]] && return 0
     [[ $ADDINS_STR == *4.4* ]] && return 0
     echo --disable-libshaderc
 }
